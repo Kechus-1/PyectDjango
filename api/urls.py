cf5c2411c879541042c03efd21eeb8f8eac6d0b0
Jafet-JafosCorporation <jafosgamer@gmail.com>
2026-07-07 21:56:07 -0600

    Fix: Reordenamiento de rutas para evitar conflicto de str:id con crear

--- api/urls.py
+++ api/urls.py
@@ -4,10 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.inicio, name='inicio'),
     path('productos/', views.listar_productos, name='listar_productos'),
-    path('productos/<str:id>/', views.detalle_producto, name='detalle_producto'),
-    path('clientes/', views.listar_clientes, name='listar_clientes'),
-    path('ventas/', views.listar_ventas, name='listar_ventas'), 
     path('productos/crear/', views.crear_producto, name='crear_producto'),
+    path('productos/<str:id>/', views.detalle_producto, name='detalle_producto'),
     path('productos/actualizar/<str:id>/', views.actualizar_producto, name='actualizar_producto'),
     path('productos/eliminar/<str:id>/', views.eliminar_producto, name='eliminar_producto'),
+    path('clientes/', views.listar_clientes, name='listar_clientes'),
+    path('ventas/', views.listar_ventas, name='listar_ventas'), 
 ]
